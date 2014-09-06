74a01417691af8e6665ee6038dd907caf0e47f3b
LFilips <luca.filipponi89@gmail.com>
2014-09-06 16:36:51 +0200

    Example Commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,11 +3,24 @@
  */
 public class Test {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         System.out.println("Starting BenchMark");
 
+        long elapsedtime;
+        long time = System.currentTimeMillis();
 
+
+        for (int i = 0; i < 1000; i++) {
+
+            System.out.println("printing ...");
+
+
+        }
+
+        elapsedtime = System.currentTimeMillis() - time;
+
+        System.out.println("Time Elapsed: " + elapsedtime);
 
 
     }
